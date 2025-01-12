178e1c6d477ece2c31bd8b088e49ee7265b917f2
Tyler Breau <tylerbreau@hotmail.com>
2025-01-12 23:31:17 +0000

    Renamed Work Biography into Work Experience. Abstracted Work Experience into component files. Tweaked and simplified the less and code for WorkExperience. Touched up the Experience Summary and Employment History.

--- src/views/screens/HomeScreen.tsx
+++ src/views/screens/HomeScreen.tsx
@@ -7,6 +7,7 @@ import {Footer} from '@Views/components/Footer';
 import {Header} from '@Views/components/Header';
 import {WorkExample} from '@Views/components/WorkExample';
 import {AboutMe} from '@Views/components/AboutMe';
+import {WorkExperience} from '@Views/components/WorkExperience';
 import {ScreenURL} from '@Logic/utility/ScreenURL';
 import {IHomeScreen, IHomeScreenState} from '@Interfaces/screens/IHomeScreen';
 
@@ -32,7 +33,7 @@ export class HomeScreen extends React.Component<{}, IHomeScreenState> implements
                         }
                     },
                     {
-                        text: 'Work Biography',
+                        text: 'Work Experience',
                         isActive: this.state.index === 1,
                         onClick: () => {
                             this.$logic.setState({
@@ -62,67 +63,7 @@ export class HomeScreen extends React.Component<{}, IHomeScreenState> implements
                     },
                     {
                         render: () => {
-                            return <div className='slideshow-content biography' key='biography'>
-                                <div className='entry summary'>
-                                    <div className='row'>
-                                        <span className='label'>Experience Summary</span>
-                                    </div>
-                                    <div className='row notes'>
-                                        <span className='label'>My work experience is focused on:</span>
-                                        <ul>
-                                            <li>React JS + JSX</li>
-                                            <li>Cordova (including various mobile features such as accelerometer and geolocation)</li>
-                                            <li>Node + TypeScript</li>
-                                            <li>MySQL</li>
-                                            <li>HTML + CSS + JavaScript</li>
-                                            <li>Less</li>
-                                            <li>SQLite</li>
-                                            <li>Flux Architecture + OOP Design</li>
-                                        </ul>
-                                        <span className='label'>I also have work experience in:</span>
-                                        <ul>
-                                            <li>PHP</li>
-                                            <li>PHP Mailer</li>
-                                            <li>TCPDF Library</li>
-                                            <li>D3 (Data-Driven Documents) Library</li>
-                                            <li>Cordova Plugin Development (Native Android and iOS)</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className='entry'>
-                                    <div className='row title-timeframe'>
-                                        <span className='label'>Web and Mobile Developer</span>
-                                        <span className='timeframe'>May 22, 2018-Present</span>
-                                    </div>
-                                    <span className='row company'>TotalPave</span>
-                                    <span className='row location'>Fredericton, NB</span>
-                                    <div className='row notes'>
-                                        <ul>
-                                            <li><b>Worked</b> with React JS, Cordova, Node, TypeScript, Less, and Webpack to build and maintain the company&apos;s websites, mobile apps, and backend systems.</li>
-                                            <li><b>Used</b> the Smart Phone features, Accelerometer, Geolocation, Camera, SQLite, and GoogleMaps APIs for iOS 9+ and Android 4.1+.</li>
-                                            <li><b>Generated</b> and <b>provided</b> charts and graphs using D3 (Data Driven Documents) library.</li>
-                                            <li><b>Designed</b> mobile apps and websites using Flux Architecture and OOP design patterns.</li>
-                                            <li><b>Followed</b> existing coding guidelines and wrote re-usable APIs.</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className='entry'>
-                                    <div className='row title-timeframe'>
-                                        <span className='label'>Web and Mobile Developer</span>
-                                        <span className='timeframe'>Summer 2016-2017</span>
-                                    </div>
-                                    <span className='row company'>Neil Squire Society</span>
-                                    <span className='row location'>Moncton, NB</span>
-                                    <div className='row notes'>
-                                        <ul>
-                                            <li><b>Worked</b> with HTML, CSS, JavaScript, PHP, and MySQL to build a website on an Apache server with MariaDB. I also used the PuTTy, HeidiSQL, PHPMyAdmin, and WinSCP tools while building the website.</li>
-                                            <li><b>Supported</b> Assistive Technology and browsers as old as IE 9 throughout the website.</li>
-                                            <li><b>Implemented</b> automated emails with PHP Mailer.</li>
-                                            <li><b>Generated</b> and <b>provided</b> downloadable PDFs with the TCPDF Library.</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>;
+                            return <WorkExperience key="WorkExperience" className="slideshow-content" />;
                         }
                     },
                     {
