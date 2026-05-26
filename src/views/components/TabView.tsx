3e234504f7d0ebac2a3a6b54a192620954e7270d
Tyler Breau <tylerbreau@hotmail.com>
2026-05-26 20:00:37 +0000

    Updated TabView for mobile support.

--- src/views/components/TabView.tsx
+++ src/views/components/TabView.tsx
@@ -7,26 +7,34 @@ import {
     ITabViewProps,
     ITabViewItem
 } from "@Interfaces/components/ITabView";
+import {DividerType} from "@Interfaces/components/IDivider";
 
 import {SlideShow} from "./SlideShow";
+import {Divider} from "@Views/components/Divider";
 
 export function TabView(props: ITabViewProps) {
     let [index, setIndex] = useState(props.items[0].index);
 
     return <div className='TabView'>
         <div className='TabViewHeader'>
-            {props.items.map((item: ITabViewItem) => {
+            {props.items.map((item: ITabViewItem, arrayIndex: number) => {
                 let className: string = 'headerItem';
                 if (item.index === index) {
                     className += ' active';
                 }
-                return <span
-                    key={"headerItem-" + item.index}
-                    className={className}
-                    onClick={() => setIndex(item.index)}
-                >
-                    {item.title}
-                </span>;
+                let out = [
+                    <span
+                        key={"headerItem-" + item.index}
+                        className={className}
+                        onClick={() => setIndex(item.index)}
+                    >
+                        {item.title}
+                    </span>
+                ];
+                if (props.items.length -1 !== arrayIndex) {
+                    out.push(<Divider type={DividerType.LINE} />);
+                }
+                return out;
             })}
         </div>
         <SlideShow
