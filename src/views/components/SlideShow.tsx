e614ea0415e05b5e0c265705ffaab8d37016e27c
Tyler Breau <tylerbreau@hotmail.com>
2026-06-12 16:09:15 +0000

    Fixed white space below slide shows due to siblings having greater height.

--- src/views/components/SlideShow.tsx
+++ src/views/components/SlideShow.tsx
@@ -1,15 +1,42 @@
 
+import {RefObject, useLayoutEffect, useRef, useState} from "react";
+
 import '@Styles/components/SlideShow.less';
 
 import {ISlideShowProps} from '@Interfaces/components/ISlideShow';
 
 export function SlideShow(props: ISlideShowProps) {
-    return <div className='SlideShow'>
+    let [maxHeight, setMaxHeight] = useState<string>('100%');
+    let refMap: Record<number, RefObject<HTMLDivElement | null>> = {};
+    for (let i = 0, length = props.items.length; i < length; ++i) {
+        refMap[i] = useRef<HTMLDivElement | null>(null);
+    }
+
+    useLayoutEffect(() => {
+        let activeSlide: HTMLDivElement | null = refMap[props.index].current;
+        if (!activeSlide) return;
+
+        if (activeSlide.clientHeight + 'px' !== maxHeight) {
+            setMaxHeight(activeSlide.clientHeight + 'px');
+        }
+    }, [props.index, maxHeight, setMaxHeight]);
+
+    return <div
+        className='SlideShow'
+        style={{
+            maxHeight: maxHeight
+        }}
+    >
         {props.items.map((item, i) => {
+            let className = 'slideshow-item';
+            if (props.index === i) {
+                className += ' active';
+            }
             return <div
                 key={i}
-                className='slideshow-item'
+                className={className}
                 style={{left: props.index * -100 + '%'}}
+                ref={refMap[i]}
             >
                 {item.render()}
             </div>;
