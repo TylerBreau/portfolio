3ab6084458fe479958e036cdb4bbabfacf535f04
Tyler Breau <tylerbreau@hotmail.com>
2026-07-30 18:50:21 +0000

    Moved copy-statics command into dedicated scripts file due to the length of the command.

---
+++ scripts/copyStatics.sh
@@ -0,0 +1,5 @@
+mkdir -p www
+cp ./src/index.html ./www/index.html
+cp ./src/res/favicon.ico ./www/favicon.ico
+cp ./src/res/FluxArchitectureImage.png ./www/FluxArchitectureImage.png 
+cp ./src/res/ProfileImage.jpg ./www/ProfileImage.jpg
